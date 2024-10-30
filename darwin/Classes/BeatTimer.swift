c8325739e600f6c82893f79649072ed9a76ca841
jeongmin <silverstar456@yonsei.ac.kr>
2024-10-30 09:17:36 +0900

    init: tick counter for ios

--- darwin/Classes/BeatTimer.swift
+++ darwin/Classes/BeatTimer.swift
@@ -1,28 +1,66 @@
 class BeatTimer {
     private var eventTick: EventTickHandler
+    private var beatCount: Int = 0
+    private var isCounting: Bool = false
+    private var resultHandler: ((Int) -> Void)?
     
     init(eventTick: EventTickHandler) {
         self.eventTick = eventTick
     }
-    private var beatTimer : Timer? = nil {
-       willSet {
-           beatTimer?.invalidate()
-       }
-    }
-    func startBeatTimer(bpm: Int) {
-        stopBeatTimer()
-        let timerIntervallInSamples = 60 / Double(bpm)
-//        var now1 = Date().timeIntervalSince1970
-        beatTimer = Timer.scheduledTimer(withTimeInterval: timerIntervallInSamples, repeats: true) { timer in
-//            let now2 = Date().timeIntervalSince1970
-//            print("tick1:"+String(describing: timerIntervallInSamples)+",diff:"+String(describing: (now2-now1)))
-//            now1 = now2
-            self.eventTick.send(res: true)
+    
+    private var beatTimer: Timer? = nil {
+        willSet {
+            beatTimer?.invalidate()
         }
     }
+    
+    func startBeatTimer(bpm: Int, onResult: @escaping (Int) -> Void) {
+        stopBeatTimer()
+        self.resultHandler = onResult
+        let timerIntervallInSamples = 60 / Double(bpm)
+        
+        beatTimer = Timer.scheduledTimer(withTimeInterval: timerIntervallInSamples, repeats: true) { [weak self] timer in
+            guard let self = self else { return }
+            self.eventTick.send(res: true)
+            
+            if self.isCounting {
+                self.beatCount += 1
+            }
+        }
+    }
+    
     func stopBeatTimer() {
         guard beatTimer != nil else { return }
         beatTimer?.invalidate()
         beatTimer = nil
+        resetCount()
+    }
+    
+    func handleStateChange(isStart: Bool, isEnd: Bool) {
+        if isStart {
+            isCounting = true
+            beatCount = 0
+        }
+        
+        if isEnd && isCounting {
+            isCounting = false
+            resultHandler?(beatCount)
+        }
+    }
+    
+    private func resetCount() {
+        beatCount = 0
+        isCounting = false
+    }
+
+    // 현재 카운트 값을 반환하는 프로퍼티 추가
+    var currentCount: Int {
+        return beatCount
+    }
+    
+    // 카운팅 중인지 여부를 반환하는 프로퍼티 추가
+    var isCountingActive: Bool {
+        return isCounting
     }
 }
+
--- lib/metronome.dart
+++ lib/metronome.dart
@@ -33,6 +33,36 @@ class Metronome {
     );
   }
 
+  /// Start counting beats
+  Future<void> startCounting() async {
+    return MetronomePlatform.instance.startCounting();
+  }
+
+  /// Stop counting beats and get the final count
+  Future<void> stopCounting() async {
+    return MetronomePlatform.instance.stopCounting();
+  }
+
+  /// Get current beat count
+  Future<int> getCurrentBeatCount() async {
+    return MetronomePlatform.instance.getCurrentBeatCount();
+  }
+
+  /// Check if beat counting is active
+  Future<bool> isCountingActive() async {
+    return MetronomePlatform.instance.isCountingActive();
+  }
+
+  /// Set a listener for beat count results
+  /// ```dart
+  /// metronome.setOnBeatCountListener((count) {
+  ///   print('Beat count: $count');
+  /// });
+  /// ```
+  void setOnBeatCountListener(Function(int count) onBeatCount) {
+    MetronomePlatform.instance.setOnBeatCountListener(onBeatCount);
+  }
+
   ///play the metronome
   Future<void> play(int bpm) async {
     return MetronomePlatform.instance.play(bpm);
